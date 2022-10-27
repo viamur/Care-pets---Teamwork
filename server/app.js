dfb0a060a7360a5f455f2abd0b68d584c9931142
Moskalenko Serhii <mynameserhii@gmail.com>
2022-10-27 16:12:43 +0300

    add start file

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const publicDirPath = path.join(__dirname, 'public');
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(publicDirPath));
+
+/* Тут роуты подключаем  */
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+requires('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const uriDB = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDB, {
+  useNewUrlParser: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
