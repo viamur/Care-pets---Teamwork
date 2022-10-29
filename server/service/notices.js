9861fd4245dec5a3536440067623afa86b86b190
Moskalenko Serhii <mynameserhii@gmail.com>
2022-10-29 16:45:15 +0300

    75%

--- server/service/notices.js
+++ server/service/notices.js
@@ -22,9 +22,29 @@ const delUserPets = async ({ owner, id }) => {
   return result;
 };
 
-const getFavorites = async ({ id }) => {};
-const addFavorites = async ({ id, userId }) => {};
-const delFavorites = async ({ id, userId }) => {};
+const getFavorites = async ({ id }) => {
+  const result = await Notices.find({ favorite: id });
+  return result;
+};
+const addFavorites = async ({ id, userId }) => {
+  const notices = await Notices.findById(id);
+  const result = await Notices.findByIdAndUpdate(
+    id,
+    { favorite: [...notices.favorite, userId] },
+    { new: true }
+  );
+  return result;
+};
+const delFavorites = async ({ id, userId }) => {
+  const notices = await Notices.findById(id);
+  const newNotices = notices.favorite.filter(el => el !== userId);
+  const result = await Notices.findByIdAndUpdate(
+    id,
+    { favorite: [...newNotices.favorite] },
+    { new: true }
+  );
+  return result;
+};
 
 module.exports = {
   getAll,
