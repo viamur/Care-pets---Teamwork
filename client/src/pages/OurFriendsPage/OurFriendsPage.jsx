cd14a746b28054d0c1ab1583689e4fa4c8c9fb37
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-10-30 14:41:18 +0100

    Start Modal

--- client/src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ client/src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -3,34 +3,26 @@ import axios from 'axios';
 
 import { useEffect, useState } from 'react';
 
+import Modal from '../../components/OurFriends/Modal/Modal';
 axios.defaults.baseURL = 'http://localhost:3005/';
 
 // import { fetchQueryPhotos } from '../api/pixabay-api';
 // import ImageGallery from './ImageGallery/ImageGallery';
 // import Searchbar from './Searchbar/Searchbar';
 // import Button from './Button/Button';
-// import Modal from './Modal/Modal';
 // import { Bars } from 'react-loader-spinner';
 
 const OurFriendsPage = () => {
   const [friends, setFriends] = useState([]);
+  const [modalTime, setModalTime] = useState(null);
 
   const fetchQueryPhotos = () => {
     return axios.get('friends').then(response => response.data);
   };
 
-  // const handleFormSubmit = query => {
-  //   setQuery(query);
-  //   setPage(1);
-  // };
-
-  // const updatePage = () => {
-  //   setPage(page => page + 1);
-  // };
-
-  // const funcModalFoto = (modalFoto = null) => {
-  //   setModalFoto(modalFoto);
-  // };
+  const funcModalTime = (modalTime = null) => {
+    setModalTime(modalTime);
+  };
 
   useEffect(() => {
     // setLoading(true);
@@ -42,7 +34,7 @@ const OurFriendsPage = () => {
     // .finally(() => setLoading(false));
   }, []);
 
-  console.log(friends);
+  // console.log(modalTime);
   return (
     <>
       <h1 className={s.mainTitle}>Our friend</h1>
@@ -51,33 +43,37 @@ const OurFriendsPage = () => {
           <li key={friend._id} className={s.card}>
             <h2 className={s.cardTitle}>{friend.title}</h2>
             <div className={s.iconDescriptionWrapper}>
-            <img
-              src={friend.icon}
-              className={s.icon}
-              alt=""
-            />
-            <ul className={s.discriptionList}>
-              <li className={s.discriptionEl}>
-                <p className={s.discription}>Time:</p>
-                <p className={s.discription}>{friend.time}</p>
-              </li>
-              <li className={s.discriptionEl}>
-                <p className={s.discription}>Adress:</p>
-                <p className={s.discription}>{friend.adress}</p>
-              </li>
-              <li className={s.discriptionEl}>
-                <p className={s.discription}>Email:</p>
-                <p className={s.discription}>{friend.email}</p>
-              </li>
-              <li className={s.discriptionEl}>
-                <p className={s.discription}>Phone:</p>
-                <p className={s.discription}>{friend.phone}</p>
-              </li>
+              <img src={friend.icon} className={s.icon} alt="Our Friend Icon" />
+              <ul className={s.discriptionList}>
+                <li
+                  className={s.discriptionEl}
+                  onClick={() => funcModalTime(friend.time)}
+                >
+                  <p className={s.discription}>Time:</p>
+                  <div className={s.time}>
+                    <p className={s.discription}>{friend.time}</p>
+                  </div>
+                </li>
+                <li className={s.discriptionEl}>
+                  <p className={s.discription}>Adress:</p>
+                  <p className={s.discription}>{friend.adress}</p>
+                </li>
+                <li className={s.discriptionEl}>
+                  <p className={s.discription}>Email:</p>
+                  <p className={s.discription}>{friend.email}</p>
+                </li>
+                <li className={s.discriptionEl}>
+                  <p className={s.discription}>Phone:</p>
+                  <p className={s.discription}>{friend.phone}</p>
+                </li>
               </ul>
-              </div>
+            </div>
           </li>
         ))}
       </ul>
+      {modalTime && (
+        <Modal modalTime={modalTime} setModalTime={funcModalTime} />
+      )}
     </>
   );
 };
