794da84de7c004e2ff630c08944a74a68ae29477
Konstantin Popov <popov.k.g@gmail.com>
2022-10-30 13:41:32 +0200

    styled news, add 2 colors vars, fixed mob in mixin

--- client/src/pages/NewsPage/NewsPage.jsx
+++ client/src/pages/NewsPage/NewsPage.jsx
@@ -1,7 +1,189 @@
 import s from './NewsPage.module.scss';
 
 const NewsPage = () => {
-  return <></>;
+  return (
+    <>
+      <h1 className={s.title}>News</h1>
+      {/* need to use  NoticeSearch Component*/}
+      <form className={s.form}>
+        <input type="text" placeholder="Search" className={s.input}></input>
+        <button type="submit" className={s.btn}></button>
+      </form>
+      {/* NEWS body */}
+      <ul className={s.list}>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            iste earum accusantium natus unde ad nulla, animi maiores in
+            corporis.
+          </h2>
+          <p className={s.text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis iure
+            magni perferendis voluptatem ex? Quasi, dolorum nemo. Amet voluptate
+            praesentium mollitia illo sit suscipit consequatur, optio accusamus
+            eaque iste fugit, exercitationem animi saepe eos nam veniam est. Qui
+            ab, deserunt beatae asperiores veniam voluptatum saepe perspiciatis
+            cumque vero? Quam praesentium, minus voluptatem neque, in dicta
+            accusantium vero, eos est necessitatibus atque dignissimos sit quis
+            consequatur! Distinctio praesentium illum vel, nostrum nihil esse
+            voluptates cum molestiae nisi blanditiis excepturi facere sint
+            velit? Praesentium suscipit earum voluptatum voluptatibus eaque,
+            facere, distinctio impedit quo, laudantium laborum error. Quibusdam
+            ipsam quas impedit minus dolore!
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+        <li className={s.item}>
+          <h2 className={s.subtitle}>Обережно, кліщі! Як уберегти улюбленця</h2>
+          <p className={s.text}>
+            Травневі прогулянки з улюбленцем не лише приємні, але й потребують
+            пильності. З початком теплої пори року активізуються кліщі, і треба
+            бути уважним, щоб уберегти свого песика чи котика від дуже серйозних
+            неприємностей зі здоров`ям.
+          </p>
+          <div className={s.addData}>
+            <p className={s.date}>20/02/2021</p>
+            <a
+              href="https://htmlreference.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={s.link}
+            >
+              Read more
+            </a>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
