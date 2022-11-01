28baed387cdb9173ffdddbb39c1d3ec780040aa4
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-11-02 00:52:10 +0100

    + loader

--- client/src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ client/src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -2,21 +2,33 @@ import s from './OurFriendsPage.module.scss';
 import { fetchFriends } from '../../utils/api';
 import { useEffect, useState } from 'react';
 import OurFriendsList from '../../components/OurFriends/OurFriendsList';
+import { FallingLines } from 'react-loader-spinner';
 
 const OurFriendsPage = () => {
   const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetchFriends()
       .then(data => {
         setFriends(data);
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <>
       <h1 className={s.mainTitle}>Our friend</h1>
+      {loading && (
+        <FallingLines
+          color="#F59256"
+          width="100"
+          visible={true}
+          ariaLabel="falling-lines-loading"
+        />
+      )}
       <OurFriendsList friends={friends} />
     </>
   );
