a80c02a922c305327f5198d038b9daf72594633c
Moskalenko Serhii <mynameserhii@gmail.com>
2022-11-06 01:07:47 +0200

    add name button header

--- client/src/components/Header/MobileMenu/MobileMenu.jsx
+++ client/src/components/Header/MobileMenu/MobileMenu.jsx
@@ -1,4 +1,6 @@
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getUserName } from 'redux/user/userSelectrors';
 
 // import { useState } from 'react';
 // import { slide as Menu } from 'react-burger-menu';
@@ -7,10 +9,9 @@ import s from './mobileMenu.module.scss';
 // import NavMenu from '../NavMenu';
 
 const MobileMenu = ({ isUserLoggedIn }) => {
-  const getActiveLink = ({ isActive }) =>
-    isActive ? s.linkActive + ' ' + s.link : s.link;
-  const getActiveButton = ({ isActive }) =>
-    isActive ? s.buttonActive + ' ' + s.button : s.button;
+  const userName = useSelector(getUserName);
+  const getActiveLink = ({ isActive }) => (isActive ? s.linkActive + ' ' + s.link : s.link);
+  const getActiveButton = ({ isActive }) => (isActive ? s.buttonActive + ' ' + s.button : s.button);
 
   // const setMenuStatus = useState(false);
   // setMenuStatus(prev => !prev);
@@ -44,11 +45,7 @@ const MobileMenu = ({ isUserLoggedIn }) => {
         <ul className={s.auth}>
           <li>
             {!isUserLoggedIn && (
-              <NavLink
-                onClick={handleClick}
-                className={getActiveButton}
-                to="/login"
-              >
+              <NavLink onClick={handleClick} className={getActiveButton} to="/login">
                 Login
               </NavLink>
             )}
@@ -56,11 +53,7 @@ const MobileMenu = ({ isUserLoggedIn }) => {
           <li>
             {' '}
             {!isUserLoggedIn && (
-              <NavLink
-                onClick={handleClick}
-                className={getActiveButton}
-                to="/register"
-              >
+              <NavLink onClick={handleClick} className={getActiveButton} to="/register">
                 Registration
               </NavLink>
             )}
@@ -68,18 +61,13 @@ const MobileMenu = ({ isUserLoggedIn }) => {
           <li>
             {isUserLoggedIn && (
               <NavLink onClick={handleClick} className={s.account} to="/user">
-                <svg
-                  viewBox="3 0 45 30"
-                  width={38}
-                  height={23}
-                  className={s.icon}
-                >
+                <svg viewBox="3 0 45 30" width={38} height={23} className={s.icon}>
                   <path
                     fill="#fff"
                     d="M16.007 2.667c-7.363 0-13.339 5.976-13.339 13.339s5.976 13.339 13.339 13.339c7.363 0 13.339-5.976 13.339-13.339s-5.976-13.339-13.339-13.339zM16.007 8.003c2.577 0.003 4.665 2.091 4.668 4.668v0c-0.003 2.577-2.091 4.666-4.668 4.669h-0c-2.577-0.003-4.666-2.092-4.668-4.669v-0c0.003-2.577 2.091-4.665 4.668-4.668h0zM16.007 26.677c-2.707 0-5.909-1.093-8.19-3.842 2.232-1.764 5.086-2.829 8.19-2.829s5.958 1.066 8.218 2.851l-0.028-0.021c-2.281 2.749-5.482 3.842-8.19 3.842z"
                   ></path>
                 </svg>
-                Account
+                {userName ? userName : 'Account'}
               </NavLink>
             )}
           </li>
@@ -92,20 +80,12 @@ const MobileMenu = ({ isUserLoggedIn }) => {
           </li>
 
           <li>
-            <NavLink
-              className={getActiveLink}
-              onClick={handleClick}
-              to="/notices/sell"
-            >
+            <NavLink className={getActiveLink} onClick={handleClick} to="/notices/sell">
               Find pet
             </NavLink>
           </li>
           <li>
-            <NavLink
-              onClick={handleClick}
-              className={getActiveLink}
-              to="/friends"
-            >
+            <NavLink onClick={handleClick} className={getActiveLink} to="/friends">
               Our Friends
             </NavLink>
           </li>
