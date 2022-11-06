c8c7a26ec7b02ca7ddbfd9b6ec1fefba83eac27f
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-11-06 12:32:03 +0100

    fix bugs with Ann and Sergey M

--- client/src/components/PetsList/PetsList.jsx
+++ client/src/components/PetsList/PetsList.jsx
@@ -5,9 +5,13 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserPets } from '../../redux/user/userSelectrors';
 
+import { delPetInUserCard } from '../../redux/user/userOperations';
+
 const PetsList = () => {
-  // const dispatch = useDispatch();
-  // const petsList = useSelector(getUserPets);
+  const dispatch = useDispatch();
+  const petsList = useSelector(getUserPets);
+
+  // console.log(petsList);
 
   // useEffect(() => {
   //   dispatch(getUserPets());
@@ -16,7 +20,50 @@ const PetsList = () => {
   return (
     <>
       <ul className={s.list}>
-        <li className={s.card}>
+        {petsList.length > 0 &&
+          petsList.map(pet => (
+            <li key={pet._id} className={s.card}>
+              <img
+                src={`https://pet-support.herokuapp.com/${pet.imgURL}`}
+                alt="Pet Foto"
+                className={s.petFoto}
+              />
+              <div className={s.cardDiscription}>
+                <button
+                  type="button"
+                  onClick={() => {
+                    dispatch(delPetInUserCard(pet._id));
+                  }}
+                  className={s.btnDelete}
+                >
+                  <svg
+                    className={s.iconDelete}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                  >
+                    <path d="M10.5 4.5h3a1.5 1.5 0 1 0-3 0ZM9 4.5a3 3 0 1 1 6 0h6A.75.75 0 1 1 21 6h-.846l-1.808 13.257a3.75 3.75 0 0 1-3.715 3.243H9.369a3.75 3.75 0 0 1-3.715-3.243L3.846 6H3a.75.75 0 0 1 0-1.5h6Zm1.5 5.25a.75.75 0 1 0-1.5 0v7.5a.75.75 0 1 0 1.5 0v-7.5ZM14.25 9a.75.75 0 0 0-.75.75v7.5a.75.75 0 1 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75Z" />
+                  </svg>
+                </button>
+                <p className={s.categoryDiscription}>Name: {pet.name}</p>
+                <p className={s.categoryDiscription}>
+                  Date of birth: {pet.dateBidthday}
+                </p>
+                <p className={s.categoryDiscription}>Breed: {pet.breed}</p>
+                <p className={s.categoryDiscription}>Comments:{pet.comments}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
+
+export default PetsList;
+
+{
+  /* <li className={s.card}>
           <img src={photoPet} alt="Pet Foto" className={s.petFoto} />
           <div className={s.cardDiscription}>
             <button type="button" className={s.btnDelete}>
@@ -41,40 +88,5 @@ const PetsList = () => {
               Dir an die Haustür – in ganz Deutschland
             </p>
           </div>
-        </li>
-        {/* {petsList.map(pet => (
-          <li key={pet.id} className={s.card}>
-            <img src={photoPet} alt="Pet Foto" className={s.petFoto} />
-            <div className={s.cardDiscription}>
-              <button
-                type="button"
-                onClick={() => {
-                  deletePet(pet.id);
-                }}
-                className={s.btnDelete}
-              >
-                <svg
-                  className={s.iconDelete}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                >
-                  <path d="M10.5 4.5h3a1.5 1.5 0 1 0-3 0ZM9 4.5a3 3 0 1 1 6 0h6A.75.75 0 1 1 21 6h-.846l-1.808 13.257a3.75 3.75 0 0 1-3.715 3.243H9.369a3.75 3.75 0 0 1-3.715-3.243L3.846 6H3a.75.75 0 0 1 0-1.5h6Zm1.5 5.25a.75.75 0 1 0-1.5 0v7.5a.75.75 0 1 0 1.5 0v-7.5ZM14.25 9a.75.75 0 0 0-.75.75v7.5a.75.75 0 1 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75Z" />
-                </svg>
-              </button>
-              <p className={s.categoryDiscription}>Name: {pet.name}</p>
-              <p className={s.categoryDiscription}>
-                Date of birth: {pet.dateBidthday}
-              </p>
-              <p className={s.categoryDiscription}>Breed: {pet.breed}</p>
-              <p className={s.categoryDiscription}>Comments:{pet.comments}</p>
-            </div>
-          </li>
-        ))} */}
-      </ul>
-    </>
-  );
-};
-
-export default PetsList;
+        </li> */
+}
