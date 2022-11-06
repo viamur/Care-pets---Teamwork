15264e52e12cc02bb38e9b2b7a7636dc82a4e315
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-11-06 13:02:25 +0100

    css

--- client/src/components/PetsList/PetsList.jsx
+++ client/src/components/PetsList/PetsList.jsx
@@ -40,12 +40,20 @@ const PetsList = () => {
                     <path d="M10.5 4.5h3a1.5 1.5 0 1 0-3 0ZM9 4.5a3 3 0 1 1 6 0h6A.75.75 0 1 1 21 6h-.846l-1.808 13.257a3.75 3.75 0 0 1-3.715 3.243H9.369a3.75 3.75 0 0 1-3.715-3.243L3.846 6H3a.75.75 0 0 1 0-1.5h6Zm1.5 5.25a.75.75 0 1 0-1.5 0v7.5a.75.75 0 1 0 1.5 0v-7.5ZM14.25 9a.75.75 0 0 0-.75.75v7.5a.75.75 0 1 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75Z" />
                   </svg>
                 </button>
-                <p className={s.categoryDiscription}>Name: {pet.name}</p>
                 <p className={s.categoryDiscription}>
-                  Date of birth: {pet.dateBidthday}
+                  <span className={s.discriptionTitle}>Name:</span> {pet.name}
                 </p>
-                <p className={s.categoryDiscription}>Breed: {pet.breed}</p>
-                <p className={s.categoryDiscription}>Comments:{pet.comments}</p>
+                <p className={s.categoryDiscription}>
+                  <span className={s.discriptionTitle}>Date of birth:</span>{' '}
+                  {pet.dateBidthday}
+                </p>
+                <p className={s.categoryDiscription}>
+                  <span className={s.discriptionTitle}>Breed:</span> {pet.breed}
+                </p>
+                <p className={s.categoryDiscription}>
+                  <span className={s.discriptionTitle}>Comments:</span>
+                  {pet.comments}
+                </p>
               </div>
             </li>
           ))}
