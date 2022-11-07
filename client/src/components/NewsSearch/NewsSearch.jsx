e0492af7f33c649d676a4ac2cdc4f12eee562821
Konstantin Popov <popov.k.g@gmail.com>
2022-11-07 15:42:27 +0200

    added autocomplete and whitespace in search

--- client/src/components/NewsSearch/NewsSearch.jsx
+++ client/src/components/NewsSearch/NewsSearch.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 import s from './NewsSearch.module.scss';
 
-const NewsSearch = ({ onSubmit }) => {
+const NewsSearch = ({ onSubmit, news, onChange }) => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleInputClick = e => {
+    setIsOpen(true);
+  };
 
   const handleChange = e => {
-    const query = e.target.value.trim();
+    const query = e.target.value.replace(/\s\s+/g, ' ');
+
     setSearchQuery(query);
+
+    onChange(query);
+  };
+
+  const handleItemClick = e => {
+    setSearchQuery(e.target.textContent);
+    setIsOpen(!isOpen);
   };
 
   const handleSubmitClick = e => {
@@ -26,8 +39,30 @@ const NewsSearch = ({ onSubmit }) => {
         autoComplete="off"
         autoFocus
         value={searchQuery}
+        onClick={handleInputClick}
         onChange={handleChange}
       />
+      <ul className={s.autocomplete__List}>
+        {searchQuery && isOpen
+          ? news
+              .filter(el =>
+                el.title.toLowerCase().includes(searchQuery.toLowerCase())
+              )
+              .map(el => {
+                const { title, _id } = el;
+
+                return (
+                  <li
+                    className={s.autocomplete__Item}
+                    key={_id}
+                    onClick={handleItemClick}
+                  >
+                    {title}
+                  </li>
+                );
+              })
+          : null}
+      </ul>
       <button type="submit" className={s.btn}></button>
     </form>
   );
