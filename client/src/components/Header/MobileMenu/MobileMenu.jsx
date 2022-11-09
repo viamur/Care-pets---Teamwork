dbef578b12fa054e37e3fcc2c00bc5bdbea3fa94
Moskalenko Serhii <mynameserhii@gmail.com>
2022-11-09 23:54:09 +0200

    fix header add style lang btn

--- client/src/components/Header/MobileMenu/MobileMenu.jsx
+++ client/src/components/Header/MobileMenu/MobileMenu.jsx
@@ -9,15 +9,13 @@ import { useTranslation } from 'react-i18next';
 import s from './mobileMenu.module.scss';
 // import NavMenu from '../NavMenu';
 
-const MobileMenu = ({ isUserLoggedIn }) => {
+const MobileMenu = ({ isUserLoggedIn, onClose }) => {
   const userName = useSelector(getUserName);
   const userAvatar = useSelector(getUserAvatar);
   const { t } = useTranslation();
 
-  const getActiveLink = ({ isActive }) =>
-    isActive ? s.linkActive + ' ' + s.link : s.link;
-  const getActiveButton = ({ isActive }) =>
-    isActive ? s.buttonActive + ' ' + s.button : s.button;
+  const getActiveLink = ({ isActive }) => (isActive ? s.linkActive + ' ' + s.link : s.link);
+  const getActiveButton = ({ isActive }) => (isActive ? s.buttonActive + ' ' + s.button : s.button);
 
   // const setMenuStatus = useState(false);
   // setMenuStatus(prev => !prev);
@@ -28,19 +26,19 @@ const MobileMenu = ({ isUserLoggedIn }) => {
 
   const handleClick = e => {
     // e.preventDefault();
-    const id = document.getElementById('modal-root');
-    if (id.contains('modal-root')) {
-      id.remove('modal-root');
-    }
-    // if (e.code === 'Escape') {
-    //   close();
-    //   changeClass('on', 'off');
-    //   return;
-    // }
-    // if (e.target === e.currentTarget) {
-    //   close();
-    //   changeClass('on', 'off');
+    // const id = document.getElementById('modal-root');
+    // if (id.contains('modal-root')) {
+    //   id.remove('modal-root');
+    onClose();
   };
+  // if (e.code === 'Escape') {
+  //   close();
+  //   changeClass('on', 'off');
+  //   return;
+  // }
+  // if (e.target === e.currentTarget) {
+  //   close();
+  //   changeClass('on', 'off');
 
   return (
     <>
@@ -51,22 +49,14 @@ const MobileMenu = ({ isUserLoggedIn }) => {
         <ul className={s.auth}>
           <li>
             {!isUserLoggedIn && (
-              <NavLink
-                onClick={handleClick}
-                className={getActiveButton}
-                to="/login"
-              >
+              <NavLink onClick={handleClick} className={getActiveButton} to="/login">
                 {t('navigation.login')}
               </NavLink>
             )}
           </li>
           <li>
             {!isUserLoggedIn && (
-              <NavLink
-                onClick={handleClick}
-                className={getActiveButton}
-                to="/register"
-              >
+              <NavLink onClick={handleClick} className={getActiveButton} to="/register">
                 {t('navigation.registration')}
               </NavLink>
             )}
@@ -83,12 +73,7 @@ const MobileMenu = ({ isUserLoggedIn }) => {
                     className={s.img}
                   />
                 ) : (
-                  <svg
-                    viewBox="3 0 45 30"
-                    width={38}
-                    height={23}
-                    className={s.icon}
-                  >
+                  <svg viewBox="3 0 45 30" width={38} height={23} className={s.icon}>
                     <path
                       fill="#fff"
                       d="M16.007 2.667c-7.363 0-13.339 5.976-13.339 13.339s5.976 13.339 13.339 13.339c7.363 0 13.339-5.976 13.339-13.339s-5.976-13.339-13.339-13.339zM16.007 8.003c2.577 0.003 4.665 2.091 4.668 4.668v0c-0.003 2.577-2.091 4.666-4.668 4.669h-0c-2.577-0.003-4.666-2.092-4.668-4.669v-0c0.003-2.577 2.091-4.665 4.668-4.668h0zM16.007 26.677c-2.707 0-5.909-1.093-8.19-3.842 2.232-1.764 5.086-2.829 8.19-2.829s5.958 1.066 8.218 2.851l-0.028-0.021c-2.281 2.749-5.482 3.842-8.19 3.842z"
@@ -108,20 +93,12 @@ const MobileMenu = ({ isUserLoggedIn }) => {
           </li>
 
           <li>
-            <NavLink
-              className={getActiveLink}
-              onClick={handleClick}
-              to="/notices/sell"
-            >
+            <NavLink className={getActiveLink} onClick={handleClick} to="/notices/sell">
               {t('navigation.noticesPage')}
             </NavLink>
           </li>
           <li>
-            <NavLink
-              onClick={handleClick}
-              className={getActiveLink}
-              to="/friends"
-            >
+            <NavLink onClick={handleClick} className={getActiveLink} to="/friends">
               {t('navigation.friendsPage')}
             </NavLink>
           </li>
