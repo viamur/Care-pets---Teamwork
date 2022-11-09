58373188344e8caf608cbe98f4a95822ab114d30
Konstantin Popov <popov.k.g@gmail.com>
2022-11-09 20:42:08 +0200

    added loader component

--- client/src/pages/NewsPage/NewsPage.jsx
+++ client/src/pages/NewsPage/NewsPage.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { fetchNews } from '../../utils/api';
 import { showInfoMessage } from '../../utils/showMessages';
-import { useTranslation } from 'react-i18next';
+import filteArrByTitle from '../../utils/filteArrByTitle';
 import NewsList from '../../components/NewsList/NewsList';
 import NewsSearch from '../../components/NewsSearch/NewsSearch';
 import Container from '../../components/Container/Container';
+import Loader from '../../components/Loader/Loader';
 import s from './NewsPage.module.scss';
-// import { showLoadingHourglass, removeLoading } from '../../utils/showLoading';
-import filteArrByTitle from '../../utils/filteArrByTitle';
 
 const NewsPage = () => {
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const { t } = useTranslation();
 
   useEffect(() => {
-    // showLoadingHourglass('Loading ...');
-
+    setIsLoading(true);
     fetchNews()
       .then(news => {
         setNews(news);
@@ -24,8 +24,8 @@ const NewsPage = () => {
           showInfoMessage(t('errors.noNews'));
         }
       })
-      .catch(err => console.log(err));
-    // .finally(() => removeLoading());
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false));
   }, []);
 
   const onSubmitSearch = searchQuery => {
@@ -38,13 +38,15 @@ const NewsPage = () => {
 
   return (
     <Container>
-      <h1 className={s.title}>{t('titles.newsPage')}</h1>
+      <h2 className={s.title}>{t('titles.newsPage')}</h2>
 
       <NewsSearch
         onSubmit={onSubmitSearch}
         news={news}
         onChange={onInputChange}
       />
+
+      {isLoading && <Loader />}
 
       <NewsList
         news={searchQuery !== '' ? filteArrByTitle(news, searchQuery) : news}
