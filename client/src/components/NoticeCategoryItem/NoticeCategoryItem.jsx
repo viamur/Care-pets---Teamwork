8fe37206f65cebc0417ce678a908a2419d558467
GubarievaAnna <annagubarieva@gmail.com>
2022-11-10 21:17:26 +0200

    Update NoticeCategoryItem.jsx

--- client/src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ client/src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -72,7 +72,7 @@ const NoticeCategoryItem = ({ data, id, array, setArray, category: path }) => {
   };
 
   function convertAge(date) {
-    const dif = Date.now() - new Date(date.split('T')[0]);
+    const dif = Date.now() - new Date(date);
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
@@ -81,7 +81,7 @@ const NoticeCategoryItem = ({ data, id, array, setArray, category: path }) => {
     const days = Math.floor(dif / day);
     const months = Math.floor(days / 30.4);
     const years = months / 12;
-    const transformedYear = Number(years.toString().split('.')[0]);
+    const transformedYear = Number(years.toFixed(0));
     const restDivision = years.toString().split('.')[1];
     const transformedMonth = restDivision
       ? Math.floor(Number(`0.${restDivision}` * 12))
